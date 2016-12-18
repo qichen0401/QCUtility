67c10b2c60cccf7b2d34f5e5a324f1fc151e7961
Qi Chen <qichen0401@gmail.com>
2016-12-18 11:40:01 +0800

    commit

---
+++ QCUtility/Classes/Common/NSFileManager+QC.h
@@ -0,0 +1,16 @@
+//
+//  NSFileManager+QC.h
+//  Pods
+//
+//  Created by Qi Chen on 12/18/16.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSFileManager (QC)
+
++ (NSURL *)documentDirectory;
++ (NSString *)pathForDocumentDirectoryWith: (NSString *)pathComponent;
+
+@end
---
+++ QCUtility/Classes/Common/NSFileManager+QC.m
@@ -0,0 +1,21 @@
+//
+//  NSFileManager+QC.m
+//  Pods
+//
+//  Created by Qi Chen on 12/18/16.
+//
+//
+
+#import "NSFileManager+QC.h"
+
+@implementation NSFileManager (QC)
+
++ (NSURL *)documentDirectory {
+    return [[[NSFileManager defaultManager] URLsForDirectory:NSDocumentDirectory inDomains:NSUserDomainMask] firstObject];
+}
+
++ (NSString *)pathForDocumentDirectoryWith: (NSString *)pathComponent {
+    return [[[NSFileManager documentDirectory] URLByAppendingPathComponent:pathComponent] path];
+}
+
+@end
